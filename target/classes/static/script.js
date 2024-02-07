266618b032409fe9da7560870ac4132f28b95c9d
anassdabaghi <anass.debbaghi123@gmail.com>
2024-02-07 15:47:12 +0100

    Delete target directory

--- target/classes/static/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello from script.js")
-const clock = document.getElementById("clock");
-let date;
-function updateClock() {
-    date = new Date();
-    clock.innerText = date.toLocaleTimeString();
-}
-
-setInterval(updateClock, 1000);
